c0b124764dbe4caffcc4823c9dbb5d769d313d4c
Samar Singh Randhawa <98534735+Samar1110@users.noreply.github.com>
2024-03-06 14:55:13 +0530

    Update 0015_alter_user_username.py

--- members/migrations/0015_alter_user_username.py
+++ members/migrations/0015_alter_user_username.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name='user',
-            name='username',
+            name='name',
             field=models.CharField(max_length=100, unique=True),
         ),
     ]
